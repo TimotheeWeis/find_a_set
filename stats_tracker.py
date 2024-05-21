4505ae513f9bf347359f58f0750dd4152455f0fe
Timothee Weisselberger <timothee.weis@gmail.com>
2024-05-21 20:09:16 +0200

    simple stats function

--- stats_tracker.py
+++ stats_tracker.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+from tabulate import tabulate
 
 class StatsTracker:
     def __init__(self):
@@ -43,6 +45,17 @@ class StatsTracker:
         with open("stats/raw_stats/" + category + ".json", "r") as file:
             self.stats[category] = json.load(file)
 
+    def load_raw_stats(self):
+        for category in self.categories:
+            self.load_stats(category)
+
+    def add_hint(self, solution):
+        first_card = solution[0]
+        second_card = solution[1]
+        third_card = solution[2]
+        category = self.find_category(first_card, second_card, third_card)
+        self.stats[category]["total_hint_given"] += 1
+
     def find_category(self, first_card, second_card, third_card):
         """
         Supposes the match is guaranteed
@@ -57,6 +70,53 @@ class StatsTracker:
         
         return category
     
+    def mean(self, category):
+        """
+        Compute the mean of the times of the category
+        """
+        return np.mean(self.stats[category]["time_list"])
+    
+    def print_mean(self, category):
+        """
+        Print the mean of the category
+        """
+        print(f"the mean of the times of the category: {category} is {self.mean(category)}")
+
+    def first_lettres(self, s):
+        """
+        Transform the categories into smaller versions
+        """
+        segments = s.split('_')
+        res = ''.join([segment[0] for segment in segments if segment])
+        return res
+
+    def print_all_means(self):
+        """
+        Print all the means
+        """
+        category_list = [self.first_lettres(category) for category in self.categories]
+        mean_list = [[self.mean(category) for category in self.categories]]
+
+        print(tabulate(mean_list, headers=category_list, tablefmt="grid"))
+
+    def indices_min_max(self, l):
+        """
+        Return the index of the min and the max of a list
+        """
+        min_index = min(range(len(l)), key=l.__getitem__)
+        max_index = max(range(len(l)), key=l.__getitem__)
+    
+        return min_index, max_index
+
+    def best_worse_category(self):
+        """
+        Give the fastest and slowest category
+        """
+        category_list = [self.first_lettres(category) for category in self.categories][1:]
+        mean_list = [self.mean(category) for category in self.categories][1:]
+        min_index, max_index = self.indices_min_max(category_list)
+        print(f"The fastest category is: {category_list[min_index]} with a mean of: {mean_list[min_index]}")
+        print(f"The slowest category is: {category_list[max_index]} with a mean of: {mean_list[max_index]}")
 
 
     
